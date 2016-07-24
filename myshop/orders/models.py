d3313d4a5abc467c5d92908a516e52941fd22511
Dikii Grigorii <dikiigr@gmail.com>
2016-07-24 20:55:09 +0300

    Lesson 6

---
+++ myshop/orders/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from shop.models import Product
+
+
+class Order(models.Model):
+    first_name = models.CharField(verbose_name='Имя', max_length=50)
+    last_name = models.CharField(verbose_name='Фамилия', max_length=50)
+    email = models.EmailField(verbose_name='Email')
+    address =  models.CharField(verbose_name='Адрес', max_length=250)
+    postal_code = models.CharField(verbose_name='Почтовый код', max_length=20)
+    city = models.CharField(verbose_name='Город', max_length=100)
+    created = models.DateTimeField(verbose_name='Создан', auto_now_add=True)
+    updated = models.DateTimeField(verbose_name='Обновлен', auto_now=True)
+    paid = models.BooleanField(verbose_name='Оплачен', default=False)
+
+    class Meta:
+        ordering = ('-created', )
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+
+    def __str__(self):
+        return 'Заказ: {}'.format(self.id)
+
+    def get_total_cost(self):
+        return sum(item.get_cost() for item in self.items.all())
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items')
+    product = models.ForeignKey(Product, related_name='order_items')
+    price = models.DecimalField(verbose_name='Цена', max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(verbose_name='Количество', default=1)
+
+    def __str__(self):
+        return '{}'.format(self.id)
+
+    def get_cost(self):
+        return self.price * self.quantity
