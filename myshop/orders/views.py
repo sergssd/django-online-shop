41d7cc9eb0ed0ba50dc88ac3f28e35a5f065b1be
Dikii Grigorii <dikiigr@gmail.com>
2016-08-08 09:14:57 +0300

    Lesson 12

--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -32,7 +32,11 @@ def OrderCreate(request):
     if request.method == 'POST':
         form = OrderCreateForm(request.POST)
         if form.is_valid():
-            order = form.save()
+            order = form.save(commit=False)
+            if cart.cupon:
+                order.cupon = cart.cupon
+                order.discount = cart.cupon.discount
+            order.save()
             for item in cart:
                 OrderItem.objects.create(order=order, product=item['product'],
                                          price=item['price'],
